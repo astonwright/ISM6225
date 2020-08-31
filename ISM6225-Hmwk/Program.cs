833ec0efd811d5760446e4725203bf50043c6a0d
mborkland <mike.borkland@gmail.com>
2020-08-31 11:20:54 -0400

    Add project files.

---
+++ ISM6225-Hmwk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM6225_Hmwk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
